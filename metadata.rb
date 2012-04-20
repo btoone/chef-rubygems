d85184f5e5216b5e895b89d168d631fe26c4bee3
Brandon Toone <caspyin@rdlk.co>
2012-04-20 15:51:25 -0600

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Brandon Toone"
+maintainer_email "brandon@rdlk.co"
+license          "Apache 2.0"
+description      "Updates rubygems"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
